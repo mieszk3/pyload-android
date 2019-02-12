60f8c0c57bd40eda7a552405b941260f745aef9d
Mieszko Stelmach <mieszko.stelmach@gmail.com>
2019-02-12 20:15:54 +0100

    Simplified helper

--- app/src/main/java/org/apache/thrift/TBaseHelper.java
+++ app/src/main/java/org/apache/thrift/TBaseHelper.java
@@ -72,13 +72,7 @@ public final class TBaseHelper {
     }
 
     public static int compareTo(double a, double b) {
-        if (a < b) {
-            return -1;
-        } else if (b < a) {
-            return 1;
-        } else {
-            return 0;
-        }
+        return Double.compare(a, b);
     }
 
     public static int compareTo(String a, String b) {
